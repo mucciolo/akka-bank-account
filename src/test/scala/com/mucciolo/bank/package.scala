21623a8cab8d76996cc73b9cbdc4f3c8825fd715
mucciolo <diego_mucciolo@hotmail.com>
2023-10-15 23:09:14 -0300

    sourced "akka.actor.serialization-bindings" value from application.conf in tests

--- src/test/scala/com/mucciolo/bank/package.scala
+++ src/test/scala/com/mucciolo/bank/package.scala
@@ -4,12 +4,10 @@ import akka.persistence.testkit.scaladsl.EventSourcedBehaviorTestKit
 import com.typesafe.config.{Config, ConfigFactory}
 
 package object bank {
-  val Config: Config = ConfigFactory.parseString(
-    """
-      akka.actor.serialization-bindings {
-        "com.mucciolo.bank.serialization.CborSerializable" = jackson-cbor
-        "scala.math.BigDecimal" = jackson-cbor
-      }
-      """)
-    .withFallback(EventSourcedBehaviorTestKit.config)
+  val Config: Config =
+    EventSourcedBehaviorTestKit.config
+      .withValue(
+        "akka.actor.serialization-bindings",
+        ConfigFactory.defaultApplication().getValue("akka.actor.serialization-bindings")
+      )
 }
